87882d44df11299af197872cd1448b9340054e35
Parker Ryan <parkerryan@parkers-mbp.lan>
2023-08-13 15:26:34 -0700

    incorporated patterns. also updated sender for compatibility, so update that as well when you pull this

--- lightwave.ino
+++ lightwave.ino
@@ -1,57 +1,86 @@
 #include "ota.h"
 #include <FastLED.h>
-//#include "patterns.h
+#include "patterns.h"
 //#include "sml_wifi.h"
 #include <Wifi.h>
-#include <ESP32Time.h>
+//#include <ESP32Time.h>
 #include <esp_now.h>
 
 //MARK: Sizes
+#define HUGE_FIXTURE 300
 #define LRG_FIXTURE 106
 #define MED_FIXTURE 96
 #define SML_FIXTURE 60
-#define FIXTURE_SIZE LRG_FIXTURE
+#define FIXTURE_SIZE HUGE_FIXTURE
 
 #define LED_PIN 5
 #define LED_BUILTIN 2
 
 //RTC
-ESP32Time rtc(0);
+//ESP32Time rtc(0);
+
+CRGB leds[FIXTURE_SIZE];
+LightObjectPattern *pattern;
 
 //Structure example to receive data
 //Must match the sender structure
-typedef struct color_struct {
+typedef struct info_struct {
+  int pattern;
   int h;
   int s;
   int v;
 };
-color_struct clr;
+info_struct info;
+
+/*
+//MARK: WIFI Recieve and store data 
+//what the ESP recieves from the lightwave client master
+struct fromClientData {
+  bool syncPulse = 0;
+  int16_t r,g,b;
+} data;
+
+//what the ESP reports back to the lightwave client
+struct toClientData {
+  bool syncPulse = 0;
+  int16_t r,g,b;
+} report;
+*/
 
 esp_now_peer_info_t peerInfo;
       
-int h = 0;
-int s = 0;
-int v = 0;
-
+int patternType = 0;
 
 //callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&clr, incomingData, sizeof(clr));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("h: ");
-  Serial.println(clr.h);
-  Serial.print("s: ");
-  Serial.println(clr.s);
-  Serial.print("v: ");
-  Serial.println(clr.v);
-  Serial.println();
-  h = clr.h;
-  s = clr.s;
-  v = clr.v;
- 
-}
 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  memcpy(&info, incomingData, sizeof(info));
+
+  if (info.pattern != patternType){
+    //TODO: clean up previous pattern object before attaching a new one
+    //delete pattern;
+    patternType = info.pattern;
+    switch (info.pattern) {
+    case 1:
+      pattern = new Snake(leds, FIXTURE_SIZE, 5);
+      break;
+    case 2:
+      pattern = new HueWash(leds, FIXTURE_SIZE);
+      break;
+    case 3:
+      pattern = new Sparkle(leds, FIXTURE_SIZE);
+      break;
+    default:
+      pattern = new Fill(leds, FIXTURE_SIZE);
+    }
+  }
+
+  pattern->set_hsv(info.h, info.s, info.v);
+}
 
 //Colors
 CHSV CHSV_YELLOW = CHSV(64, 255, 255);
@@ -61,12 +90,6 @@ CHSV CHSV_PURPLE = CHSV(192, 255, 255);
 
 //WI_FI(BatLAN, thedarknet, 10, 0, 0, 154, 4500);
 //OTA(5000);
-
-CRGB leds[FIXTURE_SIZE];
-
-//LightObjectPattern *pattern;
-//enum PatternType patternType;
-//bool patternChanged = true;
 
 void(* resetFunc) (void) = 0; //declare power reset function
 
@@ -95,10 +118,12 @@ float arValue(float pt, float offset, int phrase_step, float atk, float atk_alph
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
+  //TODO: switch to HSV
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, FIXTURE_SIZE);
+  pattern = new Fill(leds, FIXTURE_SIZE);
+  pattern->set_hsv(100, 100, 100);
 
-  rtc.setTime(0); //i believe this sets the device to epoch time
+  //rtc.setTime(0); //i believe this sets the device to epoch time
   
   //TODO: remove all serial instructions for production run
   Serial.begin(115200);
@@ -130,55 +155,26 @@ void setup() {
   
   Serial.print("MAC Address is.. ");
   Serial.println(WiFi.macAddress());
- 
+
 }
 
-//MARK: WIFI Recieve and store data 
-//what the ESP recieves from the lightwave client master
-struct fromClientData {
-  bool syncPulse = 0;
-  int16_t r,g,b;
-} data;
 
-//what the ESP reports back to the lightwave client
-struct toClientData {
-  bool syncPulse = 0;
-  int16_t r,g,b;
-} report;
 
 void loop() {
   //OTA_CHECK_UPDATES();
   //WIFI_ATTEND_CLIENT();
   //Serial.println(rtc.getMillis() + (rtc.getSecond() * 1000) + rtc.getMinute() * 60000);
   
-  
- 
-  
-/*
   //TODO: i'm not sure if this multi-class abstraction vs. one big class with all the
     //possibly unused state variables for different patterns and a milti-function
     //setup would be less cluttered, and then this logic would just always check each loop what the patternType is and run that function from all the same class
-  if (patternChanged){
-    patternChanged = false;
-    switch (patternType) {
-    case 1:
-      pattern = new HueWash(leds, FIXTURE_SIZE);
-    default:
-      pattern = new Fill(leds, FiXTURE_SIZE);
-    }
-  }
- */
 
   EVERY_N_MILLISECONDS(20){  //TODO: alternative to delay and .show() might be FastLED.delay(1000/FRAMES_PER_SECOND);
    
     //int current_val = compute_envelope(); 
-    
-    //pattern.set_hsv(150, 150, 150); //this should be unchanging for patterns that internally manage all color state, and will change via command for others (most will have value set here by looping brightness function) 
-    //pattern.draw();
-    //fill_rainbow(leds, FIXTURE_SIZE, 150);
-    fill_solid(leds, FIXTURE_SIZE, CHSV(h, s, v)); //TODO: not actually HSV at the moment, Max MSP problem, deal with laterr
+    pattern->draw();
     FastLED.show();
     
   }
-  
+
 }
--- patterns.h
+++ patterns.h
@@ -1,101 +1,122 @@
 #pragma once
-
 #include <FastLED.h>
 
-enum PatternType {
-  None = 0,
-  HueWash = 1,
-  Fill = 2,
-  Snake = 3
-}
+//TODO: remove non crucial HSV resets from pattern changes?
 
-// abstract base class of lighting objects effects
+/* 
+  LightObjectPattern: the abstract base class of lighting objects effects 
+  -- any broad variable change required by the controller regardless of pattern must be in this class so it is accessible
+*/
 class LightObjectPattern {
 public:
-    virtual void draw();
-    
-    void fill(CHSV new_hsv){
-        fill_solid(m_leds, m_num_leds, new_hsv); 
-    }
-    
-    virtual void set_hsv(uint8_t new_h, uint8_t new_s, uint8_t new_v){
-        m_hue = new_h;
-        m_sat = new_s;
-        m_value = new_v;
-    }
+  virtual void draw() = 0;  //forces pure virtual
 
-    //any broad variable change required by the controller regardless of pattern must be in this class so it is accessible
-    //TODO: add a setUpdateRate function, to modify the rate of change of internal state values (position, hue delta)
-    
-    int m_id {0};
+  virtual void set_hsv(uint8_t new_h, uint8_t new_s, uint8_t new_v) {
+    m_hue = new_h;
+    m_sat = new_s;
+    m_val = new_v;
+  }
+
+  //TODO: add a virtual setUpdateRate function, to modify the rate of change of internal state values (position, hue delta)
+
 protected:
-    uint8_t m_hue;
-    uint8_t m_sat;
-    uint8_t m_value;
-    CRGB* m_leds;
-    int m_num_leds;
+  uint8_t m_hue = 0;
+  uint8_t m_sat = 0;
+  uint8_t m_val = 0;
+  int m_num_leds;
+  CRGB* m_leds;
 };
 
 
-//Mark: Rainbow Wash
-// a better version of fill_rainbow with soft circular change in state
-class HueWash : public LightObjectPattern{ 
+class HueWash : public LightObjectPattern {
+//a better version of fill_rainbow with soft circular change in state
 public:
   HueWash(CRGB* leds, int num_leds) {
-    m_hsv = CHSV(m_hue, 255, 50);
+    m_hue = 100;
+    m_sat = 255;
+    m_val = 50;
     m_leds = leds;
     m_num_leds = num_leds;
   }
 
   void draw() {
-    for(int i = 0; i < m_num_leds; i++) {
-        m_leds[i] = CHSV((uint8_t)(m_hue + (i * 5)), 255, 255);
+    for (int i = 0; i < m_num_leds; i++) {
+      m_leds[i] = CHSV((uint8_t)(m_hue + (i * 5)), 255, m_val);
     }
-    m_hue++; 
+    m_hue++;
   }
-
-private:
-  uint8_t m_hue {0};   
 };
 
-//MARK: Pulse
-//fill_solid(m_leds, m_num_leds, new_hsv) is a general command to fill the array with a solid color
-
-class Snake : public LightObjectPattern{
+class Fill : public LightObjectPattern {
 public:
-  Snake(CRGB* leds, int num_leds, int tail_length){
-      m_leds = leds;
-      m_num_leds = num_leds;
-      m_hsv = CHSV(random(0, 255), 255, 255);
-
-      m_tail_length = tail_length;
+  Fill(CRGB* leds, int num_leds) {
+    m_leds = leds;
+    m_num_leds = num_leds;
   }
 
-  void update(){
+  void draw() {
+    fill_solid(m_leds, m_num_leds, CHSV(m_hue, m_sat, m_val));
+  }
+};
+
+
+class Snake : public LightObjectPattern {
+//a constantly forward moving set of active LEDs of a defined length
+public:
+  Snake(CRGB* leds, int num_leds, int tail_length) {
+    m_leds = leds;
+    m_num_leds = num_leds;
+    m_hue = random(0, 255);
+    m_sat = 255;
+    m_val = 255;
+    m_tail_length = tail_length;
+
+    fill_solid(m_leds, m_num_leds, CHSV(0, 0, 0));
+  }
+
+  void update() {
     m_head_idx = (m_head_idx + 1) >= (m_num_leds - 1) ? 0 : m_head_idx + 1;
   }
-  
-  void draw() override{
-    m_leds[m_head_idx] = m_hsv;
+
+  void draw() override {
+    m_leds[m_head_idx] = CHSV(m_hue, m_sat, m_val);
 
     for (int i = 1; i < m_tail_length; i++) {
       int current_pos = m_head_idx - i;
       if (current_pos >= 0 && current_pos <= m_head_idx - 1)
-        m_leds[current_pos] = m_hsv;
+        m_leds[current_pos] = CHSV(m_hue, m_sat, m_val);
     }
-    int tail_delete = (m_head_idx - m_tail_length) < 0 ? m_num_leds + (m_head_idx - m_tail_length) :  (m_head_idx - m_tail_length);
-    m_leds[tail_delete] = CHSV(0,0,0);
+    int tail_delete = (m_head_idx - m_tail_length) < 0 ? m_num_leds + (m_head_idx - m_tail_length) : (m_head_idx - m_tail_length);
+    m_leds[tail_delete] = CHSV(0, 0, 0);
     update();
   }
 
-private:  
-  int m_head_idx{0};
+private:
+  int m_head_idx{ 0 };
   int m_tail_length;
   int m_start_pos;
-  uint8_t m_hue;
-  uint8_t m_value {255};
 };
 
 
-//TODO: FastLED has a default color manipulations library for simpler stuff, should use as much of that as possible
+class Sparkle : public LightObjectPattern {
+//a randomly generated
+public:
+  Sparkle(CRGB* leds, int num_leds) {
+    m_leds = leds;
+    m_num_leds = num_leds;
+    m_hue = random(0, 255);
+    m_sat = 255;
+    m_val = 255;
+
+    fill_solid(m_leds, m_num_leds, CHSV(0, 0, 0));
+  }
+
+  void draw() override {
+    for (int i = 1; i < m_num_leds; i++) {
+      m_leds[i] = (random(2) == 0) ? CHSV(m_hue, m_sat, m_val) : CHSV(0, 0, 0);
+    }
+  }
+};
+
+//NOTE: FastLED has a default color manipulations library for simpler stuff, should use as much of that as possible
 //blend( const CRGB* src1, const CRGB* src2, CRGB* dest, uint16_t count, fract8 amountOfsrc2 ) probably useful for HueWash
